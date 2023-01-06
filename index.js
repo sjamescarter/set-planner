a84ac33b4f5bba4fee948637327a570df48db475
sjamescarter <stephenjamescarter@gmail.com>
2023-01-06 10:42:28 -0600

    write modeHandler function

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 document.addEventListener('DOMContentLoaded', init)
 
 const songsURL = "http://localhost:3000/songs"
-const setsURL = " http://localhost:3000/sets"
+const setsURL = "http://localhost:3000/sets"
 
 function init() {
     modeHandler()
-    songSearchHandler()
-    setSearchHandler()
-    populateSongs()
-    populateSets()
+    // songSearchHandler()
+    // setSearchHandler()
+    // populateSongs()
+    // populateSets()
+}
+
+// Handler Functions
+
+function modeHandler(){
+    const select = document.querySelector('#mode')
+    const plan = document.querySelectorAll('.plan')
+    const live = document.querySelector('.live')
+    
+    select.addEventListener('change', () => {
+        if(select.value === "live") {
+            plan.forEach(element => {
+                element.style.display = "none"                
+            });
+            live.style.display = "block"
+        } else {
+            plan.forEach(element => {
+                element.style.display = "block"                
+            });
+            live.style.display = "none"
+        }
+    })
 }
