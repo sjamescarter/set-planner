aba966436a4dd075aca5151d93c241621b95b204
sjamescarter <stephenjamescarter@gmail.com>
2023-01-06 14:28:08 -0600

    song search function

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const setsURL = "http://localhost:3000/sets"
 
 function init() {
     modeHandler()
-    // songSearchHandler()
+    songSearchHandler()
     // setSearchHandler()
     // populateSongs()
     // populateSets()
@@ -31,4 +31,36 @@ function modeHandler(){
             live.style.display = "none"
         }
     })
+}
+
+function songSearchHandler() {
+    const songSearchBar = document.querySelector('#song-search')
+
+    songSearchBar.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const searchBy = songSearchBar[0].value
+        const searchText = songSearchBar[1].value
+
+        get(songsURL, searchBy, searchText)
+
+        songSearchBar.reset();
+    })
+}
+
+function get(URL, searchBy, searchText) {
+    fetch(URL)
+    .then(response => response.json())
+    .then(data => data.forEach((element) => {
+        if(element[searchBy] === searchText)
+        populate(element)
+        }))
+    .catch(error => alert(error))
+}
+
+function populate(song) {
+    const songList = document.querySelector('#song-list')
+    const newSong = document.createElement('li')
+    newSong.textContent = song.title
+    songList.appendChild(newSong)
 }
