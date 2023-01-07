5b753e5f4135c30fc9fa71e1e94d4dce7de5baaa
sjamescarter <stephenjamescarter@gmail.com>
2023-01-07 12:11:15 -0600

    add clear function

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded', init)
 
-const dbURL = "http://localhost:3000/"
-const songsURL = "http://localhost:3000/songs"
-const setsURL = "http://localhost:3000/sets"
+const dbURL = "http://localhost:3000/";
+const songsURL = "http://localhost:3000/songs";
+const setsURL = "http://localhost:3000/sets";
+let activeSet;
 
 function init() {
     modeHandler()
@@ -39,11 +40,12 @@ function searchHandler() {
     searchBar.forEach((element) => {
         element.addEventListener('submit', (e) => {
             e.preventDefault();
+            clearHandler(e)
             const container = e.target.parentNode
             const searchBy = e.target[0].value
             const searchText = e.target[1].value
-            const clearUl = e.target.parentNode.querySelector('ul')
-            clearUl.innerHTML = ""
+            // const clearUl = e.target.parentNode.querySelector('ul')
+            // clearUl.innerHTML = ""
             
             get(`http://localhost:3000/${e.target.parentNode.id}`, searchBy, searchText, container)
     
@@ -53,6 +55,10 @@ function searchHandler() {
 
 
 // Callback Functions
+function clearHandler(e) {
+    const clearUl = e.target.parentNode.querySelector('ul')
+    clearUl.innerHTML = ""
+}
 
 function get(URL, searchBy, searchText, container) {
     fetch(URL)
@@ -72,20 +78,20 @@ function populate(element, container) {
 
 function createSongCard(song) {
     const songCard = document.createElement('li')
-    songCard.id = `song-${song.id}`
+    songCard.id = `songs/${song.id}`
     songCard.innerHTML = `
     <h3></h3>
     <button>+</button>
     `
     songCard.querySelector('h3').textContent = song.title
     //songCard.querySelector('h3').addEventListener('click', editSong)
-    //songCard.querySelector('button').addEventListener('click', addToList)
+    songCard.querySelector('button').addEventListener('click', addToSet)
     return songCard
 }
 
 function createSetCard(set) {
     const setCard = document.createElement('li')
-    setCard.id = `set-${set.id}`
+    setCard.id = `sets/${set.id}`
     setCard.innerHTML = `
     <h3></h3>
     <h5></h5>
@@ -96,6 +102,20 @@ function createSetCard(set) {
     return setCard
 }
 
-function editSet(e) {
-    console.log(e.target.parentNode.id)
-}
+// function editSet(e) {
+//     clearHandler(e)
+
+//     //activeSet = e.target.parentNode
+// }
+
+// function addToSet(e) {
+//     const songId = e.target.parentNode.id
+//     fetch(`${dbURL}${songId}`)
+//     .then(response => response.json())
+//     .then(data => {
+//         const newSong = document.createElement('p')
+//         newSong.textContent = data.title
+//         activeSet.appendChild(newSong)
+//     })
+//     .catch(error => alert(error))
+// }
